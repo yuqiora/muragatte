f6b95325aa605ac9c2603bed848073425c9d9c7d
jiv.yuq@gmail.com <jiv.yuq@gmail.com@4720c4e1-ff9d-e404-be3a-ea41147a6a95>
2012-08-06 09:19:26 +0000

    renaming Muragatte project to MuragatteThesis

--- Muragatte/Properties/AssemblyInfo.cs
+++ Muragatte/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Muragatte")]
+[assembly: AssemblyTitle("MuragatteThesis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Muragatte")]
+[assembly: AssemblyProduct("MuragatteThesis")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
