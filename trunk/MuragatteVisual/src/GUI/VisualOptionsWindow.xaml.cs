55a4d08cda9ea431ce6531ec7ffddd8c1234c9e6
jiv.yuq@gmail.com <jiv.yuq@gmail.com@4720c4e1-ff9d-e404-be3a-ea41147a6a95>
2012-08-07 21:16:58 +0000

    using DispatcherTimer instead of Storyboard/Animation in VisualPlaybackWindow renamed some buttons in VisualPlaybackWindow removed replay features from sandbox, obsolete due to playback window started work on VisualOptionsWindow

--- trunk/MuragatteVisual/src/GUI/VisualOptionsWindow.xaml.cs
+++ trunk/MuragatteVisual/src/GUI/VisualOptionsWindow.xaml.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// ------------------------------------------------------------------------
+// Muragatte - A Toolkit for Observation of Swarm Behaviour
+//             Visualization Library
+//
+// Copyright (C) 2012  Jiří Vejmola.
+// Developed under the MIT License. See the file license.txt for details.
+//
+// Muragatte on the internet: http://code.google.com/p/muragatte/
+// ------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +20,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Muragatte.Visual;
 
 namespace Muragatte.GUI
 {
@@ -18,9 +29,20 @@ namespace Muragatte.GUI
     /// </summary>
     public partial class VisualOptionsWindow : Window
     {
-        public VisualOptionsWindow()
+        #region Fields
+
+        private Visualization _visualization;
+
+        #endregion
+
+        #region Constructors
+
+        public VisualOptionsWindow(Visualization visualization)
         {
             InitializeComponent();
+            _visualization = visualization;
         }
+
+        #endregion
     }
 }
