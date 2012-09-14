e634dfd1471aa197e32100dd851a4e9af0a1e9ac
jiv.yuq@gmail.com <jiv.yuq@gmail.com@4720c4e1-ff9d-e404-be3a-ea41147a6a95>
2012-09-14 20:33:11 +0000

    added About window to Thesis

--- MuragatteThesis/src/GUI/ThesisAboutWindow.xaml.cs
+++ MuragatteThesis/src/GUI/ThesisAboutWindow.xaml.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,26 +20,38 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Muragatte.GUI
 {
     /// <summary>
-    /// Interaction logic for ThesisMainWindow.xaml
+    /// Interaction logic for ThesisAboutWindow.xaml
     /// </summary>
-    public partial class ThesisMainWindow : Window
+    public partial class ThesisAboutWindow : Window
     {
-        public ThesisMainWindow()
+        private const string LICENSE_PATH = "license.txt";
+
+        public ThesisAboutWindow()
         {
             InitializeComponent();
         }
 
-        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        private void btnOK_Click(object sender, RoutedEventArgs e)
         {
-            ThesisAboutWindow about = new ThesisAboutWindow();
-            about.Owner = this;
-            about.ShowDialog();
+            Close();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(((Hyperlink)sender).NavigateUri.ToString());
+        }
+
+        private void License_Click(object sender, RoutedEventArgs e)
+        {
+            if (System.IO.File.Exists(LICENSE_PATH))
+            {
+                Process.Start(LICENSE_PATH);
+            }
         }
     }
 }
